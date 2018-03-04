a9c8aa0f9f30a5f024c3c9d4996d207e3f739b63
SpencerJAl <gpmas117@gmail.com>
2018-03-04 20:02:19 +0000

    Added the adding function in javascript for the UL list in testNav.html, needs some tweeking with elementID, Please don't mess with the function as i am currently working on it

---
+++ src/js/add.js
@@ -0,0 +1,7 @@
+// Work in progress for external version of the adding function on test nav
+//function add_item_test_1(){
+	//document.getElementById("sortable").innerHTML;
+	//option = document.createElement("li");
+    //option.text = "<li class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>Item 8</li>";
+   // x.add(option);
+//}
